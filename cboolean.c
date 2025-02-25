2ddaf9e0954970a376124a436645ff8ed7e2e606
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-02-25 12:20:22 -0800

    working on the Boolean in C programming

---
+++ cboolean.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+    bool isProgrammingFun = true;
+    bool isFishTasty = false;
+    printf("%d\n", isProgrammingFun);  // Returns 1 (true)
+    printf("%d\n", isFishTasty);         // Returns 0 (false)
+  
+int myAge = 25;
+int votingAge = 18;
+
+if (myAge >= votingAge) {
+    printf("Old enough to vote!\n");
+} else {
+    printf("Not old enough to vote.\n");
+}
+    
+return 0;
+
+}
