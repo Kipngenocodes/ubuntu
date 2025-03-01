22344bf794756a6d45cadd3179b49d3d78777dcd
Kipngenocodes <kipngenoalex.c@gmail.com>
2025-02-28 18:26:45 -0800

    working with recurssions

---
+++ rescurssion.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+// Recursive function to calculate factorial
+int factorial(int n) {
+    // Base case: factorial of 0 or 1 is 1
+    if (n <= 1) {
+        return 1;
+    }
+    // Recursive case: n! = n * (n-1)!
+    else {
+        return n * factorial(n - 1);
+    }
+}
+
+int main() {
+    int num;
+    printf("Enter a number to calculate its factorial: ");
+    scanf("%d", &num);
+    
+    if (num < 0) {
+        printf("Factorial is not defined for negative numbers.\n");
+    } else {
+        printf("Factorial of %d = %d\n", num, factorial(num));
+    }
+    
+    return 0;
+}
